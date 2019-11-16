b78fb0c9725b753ad8d52a6e85a805c0aabfe42e
Tien Nguyen <toantien.vn91@gmail.com>
2019-11-16 21:58:45 +0700

    Full support UI Kitten

--- App/App.js
+++ App/App.js
@@ -8,26 +8,32 @@ import { ApplicationProvider, Layout, Text } from 'react-native-ui-kitten'
 
 const { store, persistor } = createStore()
 
+const ApplicationContent = () => (
+  <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <Text>Welcome to UI Kitten</Text>
+  </Layout>
+); 
+
 export default class App extends Component {
   render() {
     return (
       /**
        * @see https://github.com/reduxjs/react-redux/blob/master/docs/api/Provider.md
        */
-      <Provider store={store}>
-        {/**
-         * PersistGate delays the rendering of the app's UI until the persisted state has been retrieved
-         * and saved to redux.
-         * The `loading` prop can be `null` or any react instance to show during loading (e.g. a splash screen),
-         * for example `loading={<SplashScreen />}`.
-         * @see https://github.com/rt2zz/redux-persist/blob/master/docs/PersistGate.md
-         */}
-        <PersistGate loading={null} persistor={persistor}>
-          <ApplicationProvider mapping={mapping} theme={lightTheme}>
-            <RootScreen />
-          </ApplicationProvider>
-        </PersistGate>
-      </Provider>
+      <ApplicationProvider mapping={mapping} theme={lightTheme}>
+        <Provider store={store}>
+          {/**
+           * PersistGate delays the rendering of the app's UI until the persisted state has been retrieved
+           * and saved to redux.
+           * The `loading` prop can be `null` or any react instance to show during loading (e.g. a splash screen),
+           * for example `loading={<SplashScreen />}`.
+           * @see https://github.com/rt2zz/redux-persist/blob/master/docs/PersistGate.md
+           */}
+          <PersistGate loading={<ApplicationContent/>} persistor={persistor}>
+              <RootScreen />
+          </PersistGate>
+        </Provider>
+      </ApplicationProvider>
     )
   }
 }
